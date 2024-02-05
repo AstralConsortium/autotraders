711658cd6fc3cc3cc681c948671fb3c1a72f7ba8
Rafael Sillero <rafael.silnav@gmail.com>
2024-02-05 13:54:24 +0100

    Helper functions now use the right multipliers as announced in the wiki

--- autotraders/util.py
+++ autotraders/util.py
@@ -3,29 +3,45 @@ import math
 from autotraders.ship.states import FlightMode
 
 
-def travel_fuel(distance, mode):
+def travel_fuel(distance, mode) -> int:
     if mode == FlightMode.CRUISE:
-        return distance
+        return round(distance)
     elif mode == FlightMode.DRIFT:
         return 1
     elif mode == FlightMode.BURN:
-        return 2 * distance
+        return 2 * round(distance)
     elif mode == FlightMode.STEALTH:
-        return distance
+        return round(distance)
 
 
-def travel_time(distance, ship_speed, mode):
-    if mode == FlightMode.CRUISE:
-        return 15 + 15 * distance / ship_speed
-    elif mode == FlightMode.DRIFT:
-        return 15 + 150 * distance / ship_speed
-    elif mode == FlightMode.BURN:
-        return 15 + 7.5 * distance / ship_speed
-    elif mode == FlightMode.STEALTH:
-        return 15 + 30 * distance / ship_speed
+multiplier = {
+    FlightMode.CRUISE: 25,
+    FlightMode.DRIFT: 250,
+    FlightMode.BURN: 12.5,
+    FlightMode.STEALTH: 30
+}
 
 
-def distance(*args):
+def travel_time(distance, ship_speed, mode) -> int:
+    return round(round(max(1, distance)) * multiplier[mode] / ship_speed + 15)
+
+
+"""
+Take the following multipliers with a grain of salt. They have not been confirmed yet by the community and might be wrong.
+"""
+warp_multiplier = {
+    FlightMode.CRUISE: 50,
+    FlightMode.DRIFT: 300,
+    FlightMode.BURN: 25,
+    FlightMode.STEALTH: 60
+}
+
+
+def travel_time_warping(distance, ship_speed, mode) -> int:
+    return round(round(max(1, distance)) * warp_multiplier[mode] / ship_speed + 15)
+
+
+def distance(*args) -> int:
     """
     If there is one arg, then the arg should be a iterable of length 2 or 4.
     It will be expanded and used as a 2 or 4 arg.
@@ -44,4 +60,4 @@ def distance(*args):
             getattr(args[1], "y"),
         )
     else:
-        return math.sqrt((args[0] - args[2]) ** 2 + (args[1] - args[3]) ** 2)
+        return round(math.sqrt((args[0] - args[2]) ** 2 + (args[1] - args[3]) ** 2))
